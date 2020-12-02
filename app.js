ffffeb08c97af5cbbea6b95493b7d7ee903172f4
Clems-web <clement.servais1@gmail.com>
2020-12-02 15:57:11 +0100

    Finito !

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ function randomNumber() {
     return Math.trunc(Math.random() * 100);
 }
 
-for (let x = 0; x <= para.length; x++) {
+for (let x = 0; x < para.length; x++) {
     para[x].innerHTML = randomNumber().toString();
 }
